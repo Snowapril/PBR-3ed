fab520d4289bd4241fb563085f69395b483b4bbc
snowapril <sinjihng@gmail.com>
2021-08-08 13:52:03 +0900

    feat(setup) : add initial static library building project settings

---
+++ Includes/cargs/cargs.hpp
@@ -0,0 +1,24 @@
+#ifndef CARGS_CARGS_HPP
+#define CARGS_CARGS_HPP
+
+namespace cargs
+{
+class cargs
+{
+ public:
+    /**
+     * @brief Default constructor for cargs
+     */
+    cargs();
+
+    /**
+     * @brief Default destructor for cargs
+     */
+    ~cargs();
+
+ protected:
+ private:
+};
+}  // namespace cargs
+
+#endif
---
+++ Sources/cargs/cargs.cpp
@@ -0,0 +1,14 @@
+#include <cargs/cargs.hpp>
+
+namespace cargs
+{
+cargs::cargs()
+{
+    //! Do nothing
+}
+
+cargs::~cargs()
+{
+    //! Do nothing
+}
+}  // namespace cargs
